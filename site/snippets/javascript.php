58a79472242d7d81f6e05e015cc77c52b014bf46
Chris Schmidt <chris@superbilk.org>
2015-02-16 00:59:25 +0100

    jquery local

--- site/snippets/javascript.php
+++ site/snippets/javascript.php
@@ -1,11 +1,10 @@
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-
 <?php echo js(array(
+  'assets/js/jquery.min.js',
   'assets/js/bootstrap.min.js',
   'assets/js/imagesloaded.pkgd.min.js',
   'assets/js/masonry.pkgd.min.js'
 )) ?>
 
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<?php if($_SERVER["SERVER_NAME"] == 'localhost'): ?>
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<?php endif; ?>
